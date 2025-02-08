2132c6f6d425dc973e70734253612ba02a831950
Leontiy Tsagoiko <leon@192.168.1.68>
2025-02-08 15:20:25 +0300

    Upd name

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-<<<<<<< HEAD
+
   base: '/Big-and-Red-Tech/', 
   plugins: [react()],
 })
-=======
-  base: '/Big-and-Red-Tech/',
-  plugins: [react()]
-})
->>>>>>> d6aeda9 (Upd links)
